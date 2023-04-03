f9eca9c31516fed34218dfc3e68146820864443e
Tom Fletcher <thomasmatthewfletcher@gmail.com>
2023-04-03 13:20:07 +0100

    Extend API to include main FizzBuzz methods

--- src/fizzbuzz.test.ts
+++ src/fizzbuzz.test.ts
@@ -15,4 +15,38 @@ describe('FizzBuzz', () => {
     expect(fb.next()).toBe('101');
     expect(fb.next()).toBe('102');
   });
+
+  it('should replace the value with the text', () => {
+    const fb = new FizzBuzz({ values: [{ value: 3, text: 'Fizz' }] });
+    expect(fb.get(3)).toBe('Fizz');
+  });
+
+  it('should replace multiples of the value with the text', () => {
+    const fb = new FizzBuzz({ values: [{ value: 3, text: 'Fizz' }], startValue: 6 });
+    expect(fb.get(6)).toBe('Fizz');
+  });
+
+  it('should replace multiples of two values with the two texts', () => {
+    const fb = new FizzBuzz({
+      values: [
+        { value: 3, text: 'Fizz' },
+        { value: 5, text: 'Buzz' }
+      ]
+    });
+    expect(fb.get(15)).toBe('FizzBuzz');
+  });
+
+  it('should get an array of results between two values', () => {
+    const fb = new FizzBuzz({
+      values: [
+        { value: 3, text: 'Fizz' },
+        { value: 5, text: 'Buzz' },
+        { value: 7, text: 'Bang' }
+      ]
+    });
+
+    const results = fb.getBetween(100, 110);
+    const expectedResults = ['Buzz', '101', 'Fizz', '103', '104', 'FizzBuzzBang', '106', '107', 'Fizz', '109', 'Buzz'];
+    expect(results).toEqual(expectedResults);
+  });
 });
--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,15 +1,15 @@
-type FizzBuzzValue = {
+type FizzBuzzValueOption = {
   value: number,
   text: string,
 };
 
 type FizzBuzzOptions = {
-  values?: Array<FizzBuzzValue>,
+  values?: Array<FizzBuzzValueOption>,
   startValue?: number,
 };
 
 class FizzBuzz {
-  values: Array<FizzBuzzValue>;
+  values: Array<FizzBuzzValueOption>;
   nextValue: number;
 
   constructor({ values = [], startValue = 1 }: FizzBuzzOptions) {
@@ -17,9 +17,32 @@ class FizzBuzz {
     this.nextValue = startValue;
   }
 
+  get(value: number): string {
+    const texts: string[] = [];
+
+    for (const valueOption of this.values) {
+      if (value % valueOption.value === 0) {
+        texts.push(valueOption.text);
+      }
+    }
+
+    if(texts.length === 0) {
+      return value.toString();
+    }
+
+    return texts.join('');
+  }
+
+  getBetween(start: number, end: number): string[] {
+    const results: string[] = [];
+    for(let i = start; i <= end; i++) {
+      results.push(this.get(i));
+    }
+    return results;
+  }
+
   next(): string {
-    const value = this.nextValue++;
-    return value.toString();
+    return this.get(this.nextValue++);
   }
 }
 
